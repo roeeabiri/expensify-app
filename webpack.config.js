f474129c68aaefa52da2814810d423cfe058ce1e
Roee Abiri <roeeabiri@gmail.com>
2024-06-16 13:32:24 +0300

    sd

--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,7 @@ module.exports = (env) => {
     output: {
       path: path.join(__dirname, 'public'),
       filename: 'bundle.js',
+      sourceMapFilename: '[file].map', // Specify source map file name
     },
 
     module: {
